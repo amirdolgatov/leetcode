dc0f67de7936244c200ee368f7f79e21dcd72884
amir <amirdolgatov@gmail.com>
2026-04-18 18:17:20 +0300

    18.04.2026

--- 1004.cpp
+++ 1004.cpp
@@ -5,7 +5,7 @@
 class Solution 
 {
 public:
-    int longestOnes(vector<int>& nums, int k) 
+    int longestOnes(std::vector<int>& nums, int k) 
     {
         int first = 0;
         int last = 0;
@@ -15,6 +15,7 @@ public:
         int length = 0;
 
         int i = 0; 
+
         while(last < nums.size())
         {
             if (nums[last] == 0)
@@ -22,20 +23,29 @@ public:
                 ++zeroes;
             }
 
-            if (zeroes == k)
+            while(zeroes > k)
             {
-                length = last - first;
-                if (length > max)
+                if (nums[first] == 0)
                 {
-                    max = length;
+                    zeroes--;
                 }
-
-                while(nums[first] != 0)
-                {
-                    ++first;
-                }
+                first++;
             }
-            last++;
+            max = std::max(max, last - first + 1);
         }
+        return max;
     }
 };
+
+int main()
+{                          // 0 1 2 3 4 5 6 7 8 9 101112131415161718
+    std::vector<int> nums =  {0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1};
+    std::vector<int> nums2 = {1,1,1,0,0,0,1,1,1,1,0};
+    Solution solution;
+
+    // std::cout << "result = " << solution.longestOnes(nums, 2) << std::endl;
+    std::cout << "result = " << solution.longestOnes(nums2, 2) << std::endl;
+    std::cout << "result = " << solution.longestOnes(nums, 3) << std::endl;
+    // std::cout << "result = " << solution.longestOnes(nums2, 3) << std::endl;
+    return 0;
+}
